8da6d0ecdcf0252de3f6007d48173e12e164585f
Isaiah <isaiahkenny321123@gmail.com>
2025-09-22 23:23:35 -0400

    working on parsing

--- parser.go
+++ parser.go
@@ -1,7 +1,35 @@
 package main
 
 func ParseItem() {
+	// that is the string containing all of the item data
+
+	// we ned to take up until the first new line character this contains the item type
+	// we need to take everything after Item Class:
+	// depending on the type of item we get we call the respective parse function for that item type
+}
+
+func ParseRare() {
 
 }
 
-func
+// skill gems will be searched based on:
+// name after the Rarity new line
+// Level: #
+// Quality: #%
+func ParseSkillGem() {
+
+}
+
+// support gems will be searched based on:
+// name after the Rarity new line
+func ParseSupportGem() {
+
+}
+
+func ParseUnique() {
+
+}
+
+func ParseStackableCurrency() {
+
+}
